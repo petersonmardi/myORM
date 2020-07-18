ee3066baefb8531fb46c694e3f74dff5da37188b
petersonmardi <petersonmardi@gmail.com>
2020-07-18 11:58:47 -0400

    first commit

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE huawei_net (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fecha TEXT,
+    pago REAL,
+    forma_de_pago TEXT,
+    canal_de_pago TEXT,
+    cliente TEXT,
+    dirección_MAC TEXT
+    );
