5cd75edba7027e137fbb31a3373688ad3094b2bb
petersonmardi <petersonmardi@gmail.com>
2020-07-19 19:55:57 -0400

    None

--- __init__.py
+++ __init__.py
@@ -17,12 +17,6 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-def make_dicts(cursor, row):
-    return dict((cursor.decription[idx][0], values)
-                for idx, value in enumerate(row))
-
-    db.row_factory = make_dicts
-
 # Initial schemas
 def init_db():
     with app.app_context():
@@ -30,9 +24,3 @@ def init_db():
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read().decode('utf8'))
         db.commit()
-
-@app.route("/")
-def index():
-    cur = get_db().cursor()
-    cur.execute("insert into huawei_net values (1, '14-7-2020', 250.00, 'efectivo', '21', 'Sophonie', '10:f1:f2:1c:02:ee')")
-    return render_template('index.html', c="Bonjou!")
